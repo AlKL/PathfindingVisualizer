9827c4cda36f3fd1c7f3488c1a6c305e9e8a0917
Al <alexkhien@hotmail.com>
2021-03-19 07:13:58 -0400

    Added BFS

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import './Menu.css';
 import aStar from '../../algorithms/aStar';
+import bfs from '../../algorithms/bfs';
 import visualizePath from '../Menu/Visualizer';
 
 const Menu = ({ resetBoard, clearBoard, startNode, endNode }) => {
@@ -13,12 +14,19 @@ const Menu = ({ resetBoard, clearBoard, startNode, endNode }) => {
         visualizePath(aStarPath.path, aStarPath.visitedNodes);
     };
 
+    const visualizeBFS = async () => {
+        await clearBoard();
+        const bfsPath = bfs(startNode, endNode);
+        visualizePath(bfsPath.path, bfsPath.visitedNodes);
+    };
+
     return (
         <div className='menu-banner'>
             <h1>Pathfinding Visualizer</h1>
             <button onClick={resetBoard}>Reset Board</button>
             <button onClick={clearBoard}>Clear Board</button>
             <button onClick={visualizeAstar}>Visualize A*</button>
+            <button onClick={visualizeBFS}>Visualize BFS</button>
         </div>
     );
 };
