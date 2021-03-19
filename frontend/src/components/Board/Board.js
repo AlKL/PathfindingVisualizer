e1a8797fc3e4ef39051cd2d652c900c7917e1385
Al <alexkhien@hotmail.com>
2021-03-19 07:25:10 -0400

    Added move end node

--- frontend/src/components/Board/Board.js
+++ frontend/src/components/Board/Board.js
@@ -8,6 +8,7 @@ import Spot from '../Node/Spot';
 const Board = ({ grid, rows, columns, setGrid, setStartNode, setEndNode }) => {
     const [wallState, setWallState] = useState(false);
     const [startState, setStartState] = useState(false);
+    const [endState, setEndState] = useState(false);
 
     useEffect(() => {
         newBoard();
@@ -32,36 +33,6 @@ const Board = ({ grid, rows, columns, setGrid, setStartNode, setEndNode }) => {
         setEndNode(grid[3][18]);
     };
 
-    //Spot Constructor - attributes for each node
-    // function Spot(i, j) {
-    //     this.x = i;
-    //     this.y = j;
-    //     this.isStart = this.x === 3 && this.y === 2;
-    //     this.isEnd = this.x === 3 && this.y === 18;
-    //     this.g = 0;
-    //     this.f = 0;
-    //     this.h = 0;
-    //     this.neighbours = [];
-    //     this.isWall = false;
-    //     // if (Math.random(1) < 0.2) {
-    //     //     this.isWall = true;
-    //     // }
-    //     this.previous = undefined;
-    //     this.addneighbours = function (grid) {
-    //         let i = this.x;
-    //         let j = this.y;
-    //         if (i > 0) this.neighbours.push(grid[i - 1][j]);
-    //         if (i < rows - 1) this.neighbours.push(grid[i + 1][j]);
-    //         if (j > 0) this.neighbours.push(grid[i][j - 1]);
-    //         if (j < columns - 1) this.neighbours.push(grid[i][j + 1]);
-    //     };
-
-    //     //BFS attributes -> move this into the algorithm
-    //     this.d = Infinity;      //node's distance
-    //     this.parent = null;     //node's parent
-    //     this.color = null;      //node's color
-    // }
-
     const addNeighbours = (grid) => {
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < columns; j++) {
@@ -74,6 +45,8 @@ const Board = ({ grid, rows, columns, setGrid, setStartNode, setEndNode }) => {
         e.preventDefault();
         var eleArr = idToGridXY(e.target.id);
         var thisClassName = document.getElementById(e.target.id).className;
+
+        //Sets Walls
         if (e.type === 'mousedown' && !thisClassName.includes('node-start') && !thisClassName.includes('node-end')) {
             setWallState(true);
             document.getElementById(`${e.target.id}`).className = 'node node-wall';
@@ -83,8 +56,10 @@ const Board = ({ grid, rows, columns, setGrid, setStartNode, setEndNode }) => {
             grid[eleArr[0]][eleArr[1]].isWall = true;
         } else if (e.type === 'mouseup' && wallState) {
             setWallState(false);
-            // console.log(grid[eleArr[0]][eleArr[1]]);
-        } else if (e.type === 'mousedown' && thisClassName.includes('node-start')) {
+        }
+
+        //Move Start Node
+        else if (e.type === 'mousedown' && thisClassName.includes('node-start')) {
             //must set isStart/change start state/change color
             setStartState(true);
             document.getElementById(`${e.target.id}`).className = 'node';
@@ -100,6 +75,24 @@ const Board = ({ grid, rows, columns, setGrid, setStartNode, setEndNode }) => {
             setStartState(false);
             grid[eleArr[0]][eleArr[1]].isStart = true;
             setStartNode(grid[eleArr[0]][eleArr[1]]);
+        }
+
+        //Move End Node
+        else if (e.type === 'mousedown' && thisClassName.includes('node-end')) {
+            setEndState(true);
+            document.getElementById(`${e.target.id}`).className = 'node';
+            grid[eleArr[0]][eleArr[1]].isEnd = false;
+        } else if (e.type === 'mouseover' && endState && !(grid[eleArr[0]][eleArr[1]].isStart)) {
+            if (grid[eleArr[0]][eleArr[1]].isWall) {
+                grid[eleArr[0]][eleArr[1]].isWall = false;
+            }
+            document.getElementById(`${e.target.id}`).className = 'node node-end';
+        } else if (e.type === 'mouseleave' && endState && !(grid[eleArr[0]][eleArr[1]].isStart)) {
+            document.getElementById(`${e.target.id}`).className = 'node';
+        } else if (e.type === 'mouseup' && endState) {
+            setEndState(false);
+            grid[eleArr[0]][eleArr[1]].isEnd = true;
+            setEndNode(grid[eleArr[0]][eleArr[1]]);
         }
     };
 
