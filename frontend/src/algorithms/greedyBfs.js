3745eadd0a649e80766c91ffd11e3e62dcbc83dc
Al <alexkhien@hotmail.com>
2021-03-28 12:42:44 -0400

    Fixed a-star

---
+++ frontend/src/algorithms/greedyBfs.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-unused-vars */
+
+const greedyBfs = (startNode, endNode) => {
+
+
+    console.log('lul');
+
+    
+
+
+};
+
+export default greedyBfs;
